bf2eca1fa73145d6a4cf4ba02e83bc6f89129017
Wesley Sum <wesleysum0402@vt.edu>
2024-06-28 19:20:35 -0400

    conversation app setup

--- vortex/item/views.py
+++ vortex/item/views.py
@@ -7,15 +7,15 @@ from .forms import EditItemForm, NewItemForm
 
 def items(request):
     query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
     categories = Category.objects.all()
-    category_id = request.GET.get('category', '')
     items = Item.objects.filter(is_sold=False)
-
-    if query:
-        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
     if category_id:
         items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
     return render(request, 'item/items.html', {
         'items': items,
