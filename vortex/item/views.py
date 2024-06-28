0494ab973e02240e052ed2d2d30389a4b2b18442
Wesley Sum <wesleysum0402@vt.edu>
2024-06-28 18:22:17 -0400

    edit function

--- vortex/item/views.py
+++ vortex/item/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import EditItemForm, NewItemForm
 
 # Create your views here.
 def details(request, pk):
@@ -39,3 +39,21 @@ def delete(request, pk):
     item.delete()
 
     return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk): 
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item'
+    })
